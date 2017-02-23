d8fd3ac122299b916bd4bb0631935da71051a1db
SimonSun <simon.sun.dev@hotmail.com>
2017-02-24 03:35:45 +0800

    修复了根据refresh token获取新access token报{"error":"server_error","error_description":"UserDetailsService is required."}的问题

--- src/main/java/com/simon/domain/User.java
+++ src/main/java/com/simon/domain/User.java
@@ -46,17 +46,17 @@ public class User implements UserDetails{
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
