e6016ee423f5b70e8cb2ffc53509a90445195db6
SimonSun <simon.sun.dev@hotmail.com>
2017-04-14 00:16:36 +0800

    尝试从oauthserver本身解决RestTemplate访问不在同一台服务器的oauthserver，connection refused的问题。

--- src/main/java/com/simon/OauthserverApplication.java
+++ src/main/java/com/simon/OauthserverApplication.java
@@ -1,16 +1,21 @@
 package com.simon;
 
+import com.netflix.discovery.shared.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class OauthserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OauthserverApplication.class, args);
+//		SpringApplication.run(OauthserverApplication.class, args);
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
 	}
 }
