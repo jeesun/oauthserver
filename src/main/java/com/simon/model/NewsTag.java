fd39ec8952dac93c8657ec94204c49f41c0deb34
SimonSun <simon.sun.dev@hotmail.com>
2018-08-18 00:50:31 +0800

    修复代码生成器bug；生成NewsTag。

--- src/main/java/com/simon/model/NewsTag.java
+++ src/main/java/com/simon/model/NewsTag.java
@@ -1,64 +1,37 @@
 package com.simon.model;
 
+import com.simon.common.utils.SnowflakeGenId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
+import tk.mybatis.mapper.annotation.KeySql;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
+/**
+* @author SimonSun
+* @create 2018-08-18 00:41:52
+**/
+@ApiModel(description = "NewsTag")
+@Data
 @Entity
-@Table(name = "news_tag")
-public class NewsTag implements Serializable {
-    private static final long serialVersionUID = -6089611811346962418L;
+@Table(name="news_tag")
+public class NewsTag implements Serializable{
+    private static final long serialVersionUID = 1L;
+
     @Id
+    @KeySql(genId = SnowflakeGenId.class)
     @GeneratedValue(generator = "sequenceId")
     @GenericGenerator(name = "sequenceId", strategy = "com.simon.common.utils.snowflake.SequenceId")
     private Long id;
 
+    @ApiModelProperty(value = "news_info_id")
     @Column(name = "news_info_id")
     private Long newsInfoId;
 
+    @ApiModelProperty(value = "tag_id")
     @Column(name = "tag_id")
     private Long tagId;
-
-    /**
-     * @return id
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * @param id
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
-     * @return news_info_id
-     */
-    public Long getNewsInfoId() {
-        return newsInfoId;
-    }
-
-    /**
-     * @param newsInfoId
-     */
-    public void setNewsInfoId(Long newsInfoId) {
-        this.newsInfoId = newsInfoId;
-    }
-
-    /**
-     * @return tag_id
-     */
-    public Long getTagId() {
-        return tagId;
-    }
-
-    /**
-     * @param tagId
-     */
-    public void setTagId(Long tagId) {
-        this.tagId = tagId;
-    }
 }
