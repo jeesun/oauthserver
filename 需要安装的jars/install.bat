c359dca670412d4c11fe4fea9f56e97860638d9b
SimonSun <simon.sun.dev@hotmail.com>
2019-01-04 11:09:51 +0800

    添加缺失的jar。

--- 需要安装的jars/install.bat
+++ 需要安装的jars/install.bat
@@ -2,3 +2,4 @@ call mvn install:install-file -Dfile=taobao-sdk-java-auto_1455552377940-20160607
 call mvn install:install-file -Dfile=taobao-sdk-java-auto_1455552377940-20160607-source.jar -DgroupId=com.alibaba -DartifactId=dayu-source -Dversion=1.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=easyui-1.7.0.jar -DgroupId=org.webjars -DartifactId=easyui -Dversion=1.7.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=FroalaWysiwygEditor-2.8.5.jar -DgroupId=org.webjars.bower -DartifactId=FroalaWysiwygEditor -Dversion=2.8.5 -Dpackaging=jar
+call mvn install:install-file -Dfile=jeesun-adminlte-2.4.8.jar -DgroupId=org.webjars -DartifactId=jeesun-adminlte -Dversion=2.4.8 -Dpackaging=jar
