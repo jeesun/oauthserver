f563c5a2802f0be740935e0a85821ced3a0d44a6
jeesun <simon.sun.dev@hotmail.com>
2019-05-17 15:09:31 +0800

    解决悬而未决的登录失败提示。

--- web/src/main/java/com/simon/common/config/CustomLoginAuthProvider.java
+++ web/src/main/java/com/simon/common/config/CustomLoginAuthProvider.java
@@ -4,21 +4,22 @@ import com.simon.common.domain.UserEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Locale;
 
+/**
+ * @author simon
+ * @date 2019-05-17
+ */
 @Slf4j
 @Component
 public class CustomLoginAuthProvider implements AuthenticationProvider {
@@ -27,6 +28,9 @@ public class CustomLoginAuthProvider implements AuthenticationProvider {
 
     @Autowired
     private MessageSource messageSource;
+
+    @Autowired
+    private PasswordEncoder encoder;
 
     private Locale locale = AppConfig.getLocale();
 
@@ -37,17 +41,15 @@ public class CustomLoginAuthProvider implements AuthenticationProvider {
         String username = authentication.getName();
         String password = (String) authentication.getCredentials();
         UserEntity userEntity = (UserEntity) userDetailsService.loadUserByUsername(username);
-        if (null == userEntity){
+        if (null == userEntity) {
             throw new UsernameNotFoundException(messageSource.getMessage("usernameNotFound", null, locale));
         }
-        if (!userEntity.isEnabled()){
+        if (!userEntity.isEnabled()) {
             throw new DisabledException(messageSource.getMessage("accountDisabled", null, locale));
         }
 
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(11);
-
-        if(!encoder.matches(password, userEntity.getPassword())){
-            throw new InvalidGrantException(messageSource.getMessage("passwordError", null, locale));
+        if (!encoder.matches(password, userEntity.getPassword())) {
+            throw new BadCredentialsException(messageSource.getMessage("passwordError", null, locale));
         }
 
         Collection<? extends GrantedAuthority> authorities = userEntity.getAuthorities();
