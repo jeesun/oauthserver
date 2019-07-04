121ce429ad459167414acffd780bf10fcdcad132
jeesun <simon.sun.dev@hotmail.com>
2019-07-04 16:39:01 +0800

    更新全局异常处理。

--- api/src/main/java/com/simon/common/handler/GlobalExceptionHandler.java
+++ api/src/main/java/com/simon/common/handler/GlobalExceptionHandler.java
@@ -1,11 +1,10 @@
 package com.simon.common.handler;
 
 import com.simon.common.domain.ResultMsg;
+import com.simon.common.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
@@ -21,41 +20,34 @@ import javax.servlet.http.HttpServletResponse;
  * 全局异常处理类
  *
  * @author simon
- * @create 2018-04-25 23:25
+ * @date 2018-04-25 23:25
  **/
 @Slf4j
-@Configuration
-@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ExceptionHandlerExceptionResolver {
 
     @ExceptionHandler(value = {AccessDeniedException.class})
-    public ResultMsg grantAccessDeniedError(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e){
-        log.error("grantAccessDeniedError");
-        log.error(e.getMessage());
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        return ResultMsg.fail(response.getStatus(), e.getMessage());
+    public ResponseEntity<ResultMsg> grantAccessDeniedError(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ResultMsg.fail(response.getStatus(), e.getMessage()));
     }
 
     @ExceptionHandler(value = {BadCredentialsException.class})
-    public ResultMsg grantBadCredentialsError(HttpServletRequest request, HttpServletResponse response, BadCredentialsException e){
-        log.error("grantBadCredentialsError");
-        log.error(e.getMessage());
-        return ResultMsg.fail(response.getStatus(), e.getMessage());
+    public ResponseEntity<ResultMsg> grantBadCredentialsError(HttpServletRequest request, HttpServletResponse response, BadCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ResultMsg.fail(response.getStatus(), e.getMessage()));
     }
 
     @ExceptionHandler(value = {OAuth2Exception.class, InvalidGrantException.class})
-    public ResultMsg grantOAuth2Error(HttpServletRequest request, HttpServletResponse response, Exception e){
-        log.error("grantOAuth2Error");
-        log.error(e.getMessage());
-        return ResultMsg.fail(response.getStatus(), e.getMessage());
+    public ResponseEntity<ResultMsg> grantOAuth2Error(HttpServletRequest request, HttpServletResponse response, Exception e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ResultMsg.fail(response.getStatus(), e.getMessage()));
     }
 
-    /*@ExceptionHandler(value = {RuntimeException.class, Exception.class})
-    public ResultMsg grantRuntimeError(HttpServletRequest request, HttpServletResponse response, RuntimeException e){
-        log.error("grantRuntimeError");
-        log.error(e.getMessage());
-        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        return ResultMsg.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
-    }*/
+    @ExceptionHandler(value = {BusinessException.class})
+    public ResponseEntity<ResultMsg> grantBusinessError(HttpServletRequest request, HttpServletResponse response, Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResultMsg.fail(response.getStatus(), e.getMessage()));
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ResultMsg> grantRuntimeError(HttpServletRequest request, HttpServletResponse response, Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResultMsg.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
+    }
 }
