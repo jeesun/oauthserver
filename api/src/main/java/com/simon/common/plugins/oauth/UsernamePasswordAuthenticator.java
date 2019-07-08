4e107af3ec3e7e871e02a8121843d808ae485c64
jeesun <simon.sun.dev@hotmail.com>
2019-07-08 10:41:01 +0800

    修复使用refresh_token刷新access_token时，报“不支持的登录方式”的bug。

--- api/src/main/java/com/simon/common/plugins/oauth/UsernamePasswordAuthenticator.java
+++ api/src/main/java/com/simon/common/plugins/oauth/UsernamePasswordAuthenticator.java
@@ -2,7 +2,6 @@ package com.simon.common.plugins.oauth;
 
 import com.simon.common.config.AppConfig;
 import com.simon.common.domain.UserEntity;
-import com.simon.common.exception.BusinessException;
 import com.simon.common.utils.DateUtil;
 import com.simon.common.utils.ValidUtil;
 import com.simon.model.Authority;
@@ -47,15 +46,15 @@ public class UsernamePasswordAuthenticator extends AbstractPreparableIntegration
 
     private final static String PASSWORD_AUTH_TYPE = "password";
 
-    public UsernamePasswordAuthenticator(){
+    public UsernamePasswordAuthenticator() {
         sqlLoadUserByPhone = "select id,username,password,enabled,area_code,phone,email,address,birth,age,head_photo,person_brief,sex from t_users where phone=?";
         sqlLoadUserByEmail = "select id,username,password,enabled,area_code,phone,email,address,birth,age,head_photo,person_brief,sex from t_users where email=?";
         sqlLoadUserByName = "select id,username,password,enabled,area_code,phone,email,address,birth,age,head_photo,person_brief,sex from t_users where username=?";
         sqlLoadAuthorities = "select user_id,authority from t_authorities where user_id = ?";
 
-        myUserDetailsRowMapper = (rs, i) -> new UserEntity(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getBoolean(4), rs.getString("area_code"), rs.getString("phone"), rs.getString("email"),rs.getString("address"), DateUtil.dateToLocalDate(rs.getDate("birth")), rs.getInt("age"), rs.getString("head_photo"), rs.getString("person_brief"), rs.getBoolean("sex"));
+        myUserDetailsRowMapper = (rs, i) -> new UserEntity(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getBoolean(4), rs.getString("area_code"), rs.getString("phone"), rs.getString("email"), rs.getString("address"), DateUtil.dateToLocalDate(rs.getDate("birth")), rs.getInt("age"), rs.getString("head_photo"), rs.getString("person_brief"), rs.getBoolean("sex"));
 
-        authorityRowMapper = (rs,i) -> {
+        authorityRowMapper = (rs, i) -> {
             Authority authority = new Authority();
             authority.setUserId(rs.getLong(1));
             authority.setAuthority(rs.getString(2));
@@ -66,18 +65,18 @@ public class UsernamePasswordAuthenticator extends AbstractPreparableIntegration
     @Override
     public UserEntity authenticate(IntegrationAuthentication integrationAuthentication) {
         log.info("password authenticate");
-        try{
+        try {
             UserEntity userFromQuery = null;
             if (StringUtils.isEmpty(integrationAuthentication.getUsername())) {
                 throw new UsernameNotFoundException(messageSource.getMessage("usernameNotFound", null, locale));
             }
-            if(ValidUtil.isEmail(integrationAuthentication.getUsername())){
+            log.info(integrationAuthentication.getUsername());
+            if (ValidUtil.isEmail(integrationAuthentication.getUsername())) {
                 userFromQuery = jdbcTemplate.queryForObject(sqlLoadUserByEmail, myUserDetailsRowMapper, integrationAuthentication.getUsername());
-            }else if(ValidUtil.isMobile(integrationAuthentication.getUsername())){
+            } else if (ValidUtil.isMobile(integrationAuthentication.getUsername())) {
                 userFromQuery = jdbcTemplate.queryForObject(sqlLoadUserByPhone, myUserDetailsRowMapper, integrationAuthentication.getUsername());
-            }else{
-                //userFromQuery = jdbcTemplate.queryForObject(sqlLoadUserByName, myUserDetailsRowMapper, integrationAuthentication.getUsername());
-                throw new BusinessException("不支持的登录方式");
+            } else {
+                userFromQuery = jdbcTemplate.queryForObject(sqlLoadUserByName, myUserDetailsRowMapper, integrationAuthentication.getUsername());
             }
 
             log.info("查询得到用户：{}", userFromQuery);
@@ -85,7 +84,7 @@ public class UsernamePasswordAuthenticator extends AbstractPreparableIntegration
             log.info("得到其权限：{}", authorities);
 
             return new UserEntity(userFromQuery.getId(), userFromQuery.getUsername(), userFromQuery.getPassword(), userFromQuery.isEnabled(), userFromQuery.getAreaCode(), userFromQuery.getPhone(), userFromQuery.getEmail(), userFromQuery.getAddress(), userFromQuery.getBirth(), userFromQuery.getAge(), userFromQuery.getHeadPhoto(), userFromQuery.getPersonBrief(), userFromQuery.getSex(), authorities);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             log.info("查询结果集为空：{}", integrationAuthentication.getUsername());
             throw new InvalidGrantException(messageSource.getMessage("usernameNotFound", null, locale));
         }
