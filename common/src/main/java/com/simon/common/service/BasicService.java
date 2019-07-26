6007207a14ffd59e3834c0fd974d7146e0e4de7b
jeesun <simon.sun.dev@hotmail.com>
2019-07-26 16:12:04 +0800

    BasicService接口功能拆分。

--- common/src/main/java/com/simon/common/service/BasicService.java
+++ common/src/main/java/com/simon/common/service/BasicService.java
@@ -1,11 +1,6 @@
 package com.simon.common.service;
 
-import com.github.pagehelper.PageInfo;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
 import java.util.List;
-import java.util.Map;
 
 /**
  * Basic Service
@@ -14,125 +9,22 @@ import java.util.Map;
  * @date 2018-08-02
  **/
 
-public interface BasicService<T, ID> {
-    /**
-     * 统计记录总条数
-     * @return 记录总条数
-     */
-    long count();
+public interface BasicService<T, ID> extends InsertService<T, ID>, UpdateService<T, ID>, DeleteService<T, ID>, SelectService<T, ID> {
 
     /**
-     * 保存记录
+     * 新建或更新记录
      * @param model 记录
-     * @return 保存后的记录
+     * @return 批量新建或更新后的记录
      */
+    @Deprecated
     T save(T model);
 
     /**
-     * 批量保存记录
+     * 批量新建或更新记录
      * @param modelList 记录列表
-     * @return 保存后的记录列表
+     * @return 批量新建或更新后的记录列表
      */
     @Deprecated
     List<T> save(List<T> modelList);
 
-    /**
-     * PageHelper分页查询
-     * @param pageNo 页码
-     * @param pageSize 每页记录数
-     * @param orderBy 排序
-     * @return 分页数据
-     */
-    PageInfo<T> findAll(Integer pageNo, Integer pageSize, String orderBy);
-
-    /**
-     * JPA分页查询
-     * @param pageable 分页对象
-     * @return 分页数据
-     */
-    Page<T> findAll(Pageable pageable);
-
-    /**
-     * 查询所有记录
-     * @return 所有记录
-     */
-    List<T> findAll();
-
-    /**
-     * 根据id删除记录
-     * @param id 记录id
-     */
-    void delete(ID id);
-
-    /**
-     * 根据id批量删除记录
-     * @param ids 批量记录id，逗号隔开
-     * @return 影响的记录条数
-     */
-    int deleteByIds(String ids);
-
-    /**
-     * 根据id查询记录
-     * @param id 记录id
-     * @return 记录
-     */
-    T findById(ID id);
-
-    /**
-     * 批量插入记录
-     * @param list 记录列表
-     * @return 影响的记录条数
-     */
-    int insertList(List<T> list);
-
-    /**
-     * 插入记录
-     * @param model 记录
-     * @return 影响的记录条数
-     */
-    int insert(T model);
-
-    /**
-     * 可选地插入记录（只保存记录的非空属性）
-     * @param model 记录
-     * @return 影响的记录条数
-     */
-    int insertSelective(T model);
-
-    /**
-     * 根据id更新记录（更新记录的全部属性）
-     * @param model 待更新记录
-     * @return 影响的记录条数
-     */
-    int updateByPrimaryKey(T model);
-
-    /**
-     * 根据id更新记录（只更新记录的非空属性）
-     * @param model 待更新记录
-     * @return 影响的记录条数
-     */
-    int updateByPrimaryKeySelective(T model);
-
-    /**
-     * 通用查询
-     * @param params 参数
-     * @param pageNo 页码
-     * @param pageSize 每页条数
-     * @param orderBy 排序
-     * @return 分页数据
-     */
-    PageInfo<T> getList(Map<String, Object> params, Integer pageNo, Integer pageSize, String orderBy);
-
-
-    /**
-     * 批量保存
-     * @param list 要保存的数据
-     */
-    void batchSave(List<T> list);
-
-    /**
-     * 批量更新
-     * @param list 要更新的数据
-     */
-    void batchUpdate(List<T> list);
 }
