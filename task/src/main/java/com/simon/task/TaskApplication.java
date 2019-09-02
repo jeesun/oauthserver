0987e94b07ff6ecb0979c550c987c0e5906508a7
simon <simon.sun.dev@hotmail.com>
2019-09-02 13:56:38 +0800

    新建Quartz定时任务模块。

---
+++ task/src/main/java/com/simon/task/TaskApplication.java
@@ -0,0 +1,13 @@
+package com.simon.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+
+}
